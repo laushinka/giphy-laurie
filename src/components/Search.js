2045c02185fe72f0841e02f2f9ca0704080faf46
laushinka <laurie.malau@gmail.com>
2016-08-05 19:30:06 -0400

    Add Search component

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search" />
+      </form>
+    );
+  }
+};
+
+export default Search;
