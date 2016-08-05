12d5c9f9c165f39b3b25ad68fdeb353e98b72d5c
laushinka <laurie.malau@gmail.com>
2016-08-05 19:29:55 -0400

    Add Search as child of App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo                 from './logo.svg';
+// import logo                 from './logo.svg';
 import './App.css';
-import Request              from 'superagent';
+// import Request              from 'superagent';
+import Search               from './components/Search.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Search />
           <h2>HARROOOO</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
