fa3288c7b4aa6b88ed533b10edb296a5f975a1e1
laushinka <laurie.malau@gmail.com>
2016-08-06 16:49:25 -0400

    Add handleSubmit()

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,13 +7,17 @@ class SearchBar extends Component {
   }
   handleTextChange(e) {
     this.setState({keyword: e.target.value});
-    debugger
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    var keyword = this.state.keyword.trim();
+    this.setState({keyword: ""});
   }
   render() {
     return (
       <div className="search-container">
           <input className="search-bar" type="text" placeholder="Search" value={this.state.keyword} onChange={this.handleTextChange.bind(this)}/>
-          <input type="submit" />
+          <input type="submit" onSubmit={this.handleSubmit.bind(this)} />
       </div>
     );
   }
