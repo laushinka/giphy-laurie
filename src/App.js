91c5692490c31f44c7c6ef31c97611a6866b8022
laushinka <laurie.malau@gmail.com>
2016-08-06 23:07:01 -0400

    Move API calling here

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import logo                 from './logo.svg';
 import './App.css';
-import Request              from 'superagent';
+// import Request              from 'superagent';
 import SearchBar            from './components/SearchBar.js'
+import GifContainer from './components/GifContainer.js'
 
 class App extends Component {
   constructor(props) {
@@ -12,18 +13,35 @@ class App extends Component {
     }
   }
 
-  searchTerm(keyword) {
-  var url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=dc6zaTOxFJmzC`;
-  Request.get(url, function(err,res) {
-    console.log(res.body.data[0]);
-  });
+  // handle search from SearcComponent
+  handleSubmit(keyword) {
+    $.ajax({
+     url: `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=dc6zaTOxFJmzC`,
+     method: "GET",
+     success: ((data) => {
+       this.setState({gifs: data.data});
+       //var gifs = data.data[0].images.original.url
+       //debugger;
+     }),
+     error: ((data) => {
+       alert("Not successful")
+     })
+   });
   }
+
+  // searchTerm(keyword) {
+  // var url = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=dc6zaTOxFJmzC`;
+  // Request.get(url, function(err,res) {
+  //   console.log(res.body.data[0]);
+  // });
+  // }
 
   render() {
     return (
       <div>
         <h1>Gifs</h1>
-        <SearchBar />
+        <SearchBar onSubmit={this.handleSubmit.bind(this)}/>
+        <GifContainer gifs={this.state.gifs} />
       </div>
     );
   }
