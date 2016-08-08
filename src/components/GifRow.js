7d5f9569474e74d711672a326c3b2e545ea2ed8e
laushinka <laurie.malau@gmail.com>
2016-08-07 21:39:09 -0400

    Hovering attempt

--- src/components/GifRow.js
+++ src/components/GifRow.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 // import GifObject from './GifObject.js';
 
 class GifRow extends Component {
+  handleMouseOver(e) {
+    e.preventDefault();
+    this.setState({
+      isHovering: true
+    })
+  }
+  handleMouseOut(e) {
+    e.preventDefault();
+    this.setState({
+      isHovering: false
+    })
+  }
   render() {
+    var img = this.props.img;
     var gif = this.props.gif;
-    return(
-      <tr>
-        <td><img src={gif} /></td>
-      </tr>
-    );
+    debugger;
+    if (this.state.isHovering) {
+      return(
+        <tr>
+          <td><img src={gif} /></td>
+        </tr>
+      )
+    } else {
+      return(
+        <tr>
+          <td><img src={img} /></td>
+        </tr>
+      )
+    }
   }
 }
 
