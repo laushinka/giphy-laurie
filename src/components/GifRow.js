ce4309ce095358c9c886758ae7c23b0ae8035c26
laushinka <laurie.malau@gmail.com>
2016-08-08 11:33:30 -0400

    Refactor render function

--- src/components/GifRow.js
+++ src/components/GifRow.js
@@ -8,34 +8,43 @@ class GifRow extends Component {
       isHovering: false
     }
   }
-  handleMouseOver(e) {
-    e.preventDefault();
-    this.setState({
-      isHovering: true
-    })
+  handleMouseMovement(e) {
+    // e.preventDefault();
+    this.setState({isHovering: !this.state.isHovering})
   }
-  handleMouseOut(e) {
-    e.preventDefault();
-    this.setState({
-      isHovering: false
-    })
-  }
+  // handleMouseOut(e) {
+  //   e.preventDefault();
+  //   this.setState({
+  //     isHovering: false
+  //   })
+  // }
   render() {
+
     var img = this.props.img;
     var gif = this.props.gif;
-    if (this.state.isHovering) {
-      return(
-        <tr>
-          <td><img onMouseOut={this.handleMouseOut.bind(this)} src={gif} /></td>
-        </tr>
-      )
-    } else {
-      return(
-        <tr>
-          <td><img onMouseOver={this.handleMouseOver.bind(this)} src={img} /></td>
-        </tr>
-      )
-    }
+    var source = this.state.isHovering ? gif : img
+    return(
+      <tr>
+        <td>
+          <img src={source}
+          onMouseOver={this.handleMouseMovement.bind(this)}
+          onMouseOut={this.handleMouseMovement.bind(this)} />
+        </td>
+      </tr>
+    )
+    // if (this.state.isHovering) {
+    //   return(
+    //     <tr>
+    //       <td><img onMouseOut={this.handleMouseMovement.bind(this)} src={gif} /></td>
+    //     </tr>
+    //   )
+    // } else {
+    //   return(
+    //     <tr>
+    //       <td><img onMouseOver={this.handleMouseMovement.bind(this)} src={img} /></td>
+    //     </tr>
+    //   )
+    // }
   }
 }
 
